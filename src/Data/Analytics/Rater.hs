0464358495a6e9eede9032e34eef7d475b47ea8c
Jonathan Fischoff <jonathangfischoff@gmail.com>
2013-03-13 20:01:55 -0700

    beer wins

--- src/Data/Analytics/Rater.hs
+++ src/Data/Analytics/Rater.hs
@@ -6,29 +6,73 @@ import Control.Monad.Logic
 import Data.Analytics.Datalog
 import Data.Text
 import Data.Typeable
+import Data.Data
     
 data Thing = Thing {
         name     :: String
     }    
     
+data Hand = L | R deriving(Show, Eq, Typeable, Data, Ord)
+    
 data Battle = Battle {
         left   :: Thing,
         right  :: Thing,
-        winner :: Bool
+        winner :: Hand
     }    
     
 -- try to infer a total order from a partial order
 -- find the complete ranking from the pair wise rankings
 
 things  :: T1 String String () 
-battles :: T3 (String, String, Bool) String String Bool ()
+battles :: T3 (String, String, Hand) String String Hand ()
 things  = t1 (Table 0 const) (\x () -> x :: String)
-battles = t3 (Table 1 const) (\x y z () -> (x :: String, y :: String, z::Bool))
+battles = t3 (Table 1 const) (\x y z () -> (x :: String, y :: String, z::Hand))
 
 instance Term String
-data StringVar = P | Q | R | S | T | U | V deriving (Eq,Ord,Show,Typeable)
+data StringVar = P | Q | S | T | U | V deriving (Eq,Ord,Show,Typeable)
+
 instance Term StringVar where
   type Entity StringVar = String
   term = var
-  
+
+data HandVar = W deriving (Eq,Ord,Show,Typeable) 
+ 
+instance Term Hand
+instance Term HandVar where
+  type Entity HandVar = Hand
+  term = var 
+ 
+test :: Monad m => DatalogT m (Logic (String,String, Hand))
+test = do   
+    things "pizza"
+    things "beer"
+    things "sex"
+    things "drugs"
+    things "rock"
+    things "roll"
+    
+    battles "pizza" "beer" L
+    battles "beer" "sex"   R
+    battles "drugs" "beer" R
+    battles "rock" "beer"  L
+    battles "roll" "rock"  R
+    battles "drugs" "rock" R
+    
+    query $ row (battles U "beer" W)
+    
+    
+test' :: Datalog (Logic (String,String, Hand))
+test' = test    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
  
