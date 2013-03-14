6f80344051ea54c9448164d820c049d0e8aa93a5
Jonathan Fischoff <jonathangfischoff@gmail.com>
2013-03-13 19:23:04 -0700

    added cabal file

--- src/Data/Analytics/Rater.hs
+++ src/Data/Analytics/Rater.hs
@@ -1,2 +1,7 @@
-module Data.Analytics.Rater (
-) where
+module Data.Analytics.Rater where
+
+import Control.Applicative
+import Control.Monad.Logic
+import Data.Analytics.Datalog
+import Data.Text
+import Data.Typeable
